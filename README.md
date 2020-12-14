7d9050fa3b39aac377f928d376d8f150b9209f00
rickydomingos97 <ricardodomingos97@gmail.com>
2020-12-13 22:21:12 -0300

    refactoring code js folder included

--- README.md
+++
@@ -1,1 +0,0 @@
-https://img.shields.io/github/followers/rickdomingos97?style=social
---
+++ class04/js/script.js
@@ -0,0 +1,3 @@
+window.alert('Priviet! Welcome to our course!')
+window.confirm('Do you like our JS course?')
+window.prompt('Whats is your namne?')
